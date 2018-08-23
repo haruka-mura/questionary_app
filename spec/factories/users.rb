9a626125339b9762928936273f926ca74e0e94fa
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-23 18:29:16 +0900

    Add user_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name "MyString"
     sequence(:name) { |n| "みんなのウェディング#{n}" }
     sequence(:email) { |n| "TEST#{n}@example.com" }
-    password "TEST_PASSWORD"
-    password_confirmation "TEST_PASSWORD"
+    password { "TEST_PASSWORD" }
+    password_confirmation { "TEST_PASSWORD" }
   end
 end
