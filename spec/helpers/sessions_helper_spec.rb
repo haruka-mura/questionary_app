d046b86ffecf7f156d96fd1330ddacffaabb2cc9
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-09-13 14:30:02 +0900

    不要なコメントを削除

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -18,30 +18,4 @@ RSpec.describe SessionsHelper, type: :helper do
 
     it { is_expected.to change { session[:user_id] }.from(user.id).to(nil) }
   end
-  # 
-  # describe '#current_user' do
-  #   subject { helper.current_user }
-  #
-  #   let(:user) { create :user }
-  #
-  #   before { helper.log_in(user) }
-  #
-  #   it { is_expected.to eq user }
-  # end
-  #
-  # describe '#logged_in?' do
-  #   subject { helper.logged_in? }
-  #
-  #   context 'when user is logged_in' do
-  #     let(:user) { create :user }
-  #
-  #     before { helper.log_in(user) }
-  #
-  #     it { is_expected.to be_truthy }
-  #   end
-  #
-  #   context 'when user is not logged_in' do
-  #     it { is_expected.to be_falsey }
-  #   end
-  # end
 end
